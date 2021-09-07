8aeb468c1f0d10100938ed39921a0eb75110b3c6
Savvabo <savvabogun@gmail.com>
2021-09-07 14:34:49 +0300

    code refactored

--- const.py
+++ const.py
@@ -2,9 +2,12 @@ LOGIN = 'admin'
 PASSWORD = '@H|oSRIiow1YrDWB'
 IP = 'mongodb1.accelerator-introlab.ml'
 DB_NAME = 'Git_education'
+
 STARS_COUNT = '>100000'
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:73.0) Gecko/20100101 Firefox/73.0'
 }
 PROXIES = '5.189.151.227:24001'
+USER_PROXY = False
+GIT_API = 'https://api.github.com'
--- git_scraper.py
+++ git_scraper.py
@@ -1,68 +1,86 @@
-from typing import Union
-
 import requests
 import mongodb_storage
 import const
+from dataclasses import dataclass
 
-search_api_link = 'https://api.github.com/search/repositories?q=stars:{}'.format(const.STARS_COUNT)
+search_api_link = f'{const.GIT_API}/search/repositories?q=stars:{const.STARS_COUNT}'
 
 
 def get_json(link: str) -> dict:
     proxy_dict = {'http': const.PROXIES, 'https': const.PROXIES}
-    response = requests.request( # PROXY
+    response = requests.request(  # PROXY
         'GET',
         link,
         headers=const.HEADERS,
-        proxies=proxy_dict)
-    # response = requests.request(
-    #     'GET',
-    #     link,
-    #     headers=const.HEADERS)
+        proxies=proxy_dict if const.USER_PROXY else None)
     content = response.json()
     return content
 
 
-def get_language_percentage(repo_name: str) -> Union[str, dict]:
-    language_api_link = f'https://api.github.com/repos/{repo_name}/languages'
+def get_language_percentage(repo_name: str) -> dict:
+    language_api_link = f'{const.GIT_API}/repos/{repo_name}/languages'
     language_api_dict = get_json(language_api_link)
-    if len(language_api_dict) == 0:
-        return 'no programming language in the repo'
-    else:
+    language_dict = {}
+    if len(language_api_dict) != 0:
         keys = [*language_api_dict]
         values = list(language_api_dict.values())
         values_sum = sum(values)
         values = list(map(lambda x: (x * 100) / values_sum, values))
         language_dict = dict(zip(keys, values))
-        return language_dict
+    return language_dict
+
+
+def get_last_data(repo_name: str, suffix: str) -> str:
+    issue_api_link = f'{const.GIT_API}/repos/{repo_name}/{suffix}?per_page=1'
+    last_data = get_json(issue_api_link)[0]['created_at']
+    return last_data
 
 
 def get_last_issue(repo_name: str) -> str:
-    issue_api_link = f'https://api.github.com/repos/{repo_name}/issues?per_page=1'
-    last_issue = get_json(issue_api_link)[0]['created_at']
-    return last_issue
+    return get_last_data(repo_name, 'issues')
 
 
 def get_last_pull(repo_name: str) -> str:
-    pull_api_link = f'https://api.github.com/repos/{repo_name}/pulls?per_page=1'
-    last_pull = get_json(pull_api_link)[0]['created_at']
-    return last_pull
+    return get_last_data(repo_name, 'pulls')
+
+
+@dataclass
+class RepositoryData:
+    _id: str
+    full_repo_name: str
+    repo_link: str
+    repo_creation_date: str
+    repo_license: dict
+    repo_last_pull: str
+    repo_last_issue: str
+    repo_language_percentage: dict
 
 
 def get_repo_data(json_data: dict) -> list:
     repo_dicts = json_data['items']
     repos_list = list()
     for repo_dict in repo_dicts:
-        certain_repo_data = dict()
-        certain_repo_data['_id'] = repo_dict['name']
-        certain_repo_data['repo_name'] = repo_dict['name']
-        certain_repo_data['full_repo_name'] = repo_dict['full_name']
-        certain_repo_data['repo_link'] = repo_dict['html_url']
-        certain_repo_data['repo_creation_date'] = repo_dict['created_at']
-        certain_repo_data['repo_license'] = repo_dict['license']
-        certain_repo_data['repo_last_pull'] = get_last_pull(certain_repo_data['full_repo_name'])
-        certain_repo_data['repo_last_issue'] = get_last_issue(certain_repo_data['full_repo_name'])
-        certain_repo_data['repo_language_percentage'] = get_language_percentage(certain_repo_data['full_repo_name'])
-        repos_list.append(certain_repo_data)
+        repository_dataclass = RepositoryData(
+            repo_dict['name'],
+            repo_dict['full_name'],
+            repo_dict['html_url'],
+            repo_dict['created_at'],
+            repo_dict['license'],
+            get_last_pull(repo_dict['full_name']),
+            get_last_issue(repo_dict['full_name']),
+            get_language_percentage(repo_dict['full_name']),
+        )
+        repos_list.append(repository_dataclass)
+        # certain_repo_data = dict()
+        # certain_repo_data['_id'] = repo_dict['name']
+        # certain_repo_data['full_repo_name'] = repo_dict['full_name']
+        # certain_repo_data['repo_link'] = repo_dict['html_url']
+        # certain_repo_data['repo_creation_date'] = repo_dict['created_at']
+        # certain_repo_data['repo_license'] = repo_dict['license']
+        # certain_repo_data['repo_last_pull'] = get_last_pull(certain_repo_data['full_repo_name'])
+        # certain_repo_data['repo_last_issue'] = get_last_issue(certain_repo_data['full_repo_name'])
+        # certain_repo_data['repo_language_percentage'] = get_language_percentage(certain_repo_data['full_repo_name'])
+        # repos_list.append(certain_repo_data)
     return repos_list
 
 
